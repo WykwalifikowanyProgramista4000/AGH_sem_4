684fee6195538142cfcf7da086079d21d14d8424
Wojti <wdziuba@student.agh.edu.pl>
2019-04-13 20:54:07 +0200

    PA sprawko - bez wniosków

--- Podstawy Automatyki 2/Lab 3/Untitled.m
+++ Podstawy Automatyki 2/Lab 3/Untitled.m
@@ -3,8 +3,17 @@ load pomiary
 czas = linspace(0,300,length(pomiary));
 
 %% Inercja I rzêdu z opóŸnieniem
+% KOÑCOWE WARTOŒCI
+% Y0 = max(pomiary);
+% U0 = 1;
+% 
+% k = Y0/U0;
+% tau = 30;
+% n = 5;
+% T = 39.8;
 
-plot(czas,pomiary);
+plot(czas,pomiary, 'r');
+set(gcf,'position',[300, 300, 800,400])
 grid on;
 hold on
 
@@ -14,7 +23,7 @@ U0 = 1;
 k = Y0/U0;
 tau = 30;
 n = 5;
-T = 39.8;
+T = 40;
 
 [ld, md] = pade(tau, n);
 l = [k];
@@ -22,18 +31,29 @@ m = [T 1];
 [licznik,mianownik] = series(ld,md, l, m);
 Inercja_I_delay = step(licznik, mianownik, czas);
 
-plot(czas, Inercja_I_delay)
+plot(czas, Inercja_I_delay, 'm')
 
 krok = czas(2) - czas(1);
 blad2 = (Inercja_I_delay' - pomiary).^2;
 calka_z_bledu = sum(blad2)
 
 lsim([1],[1 0], blad2, czas)
+legend('Charakterystyka badanego obiektu', 'Aproksymacja modelem I rzêdu z opóŸnieniem', 'Ca³ka z kwadratu b³êdu');
 
 hold off
 %% Inercja II rzêdu z opóŸnieniem
+% 
+% Y0 = max(pomiary);
+% U0 = 1;
+% 
+% k = Y0/U0;
+% tau = 12;
+% n = 5;
+% T1 = 28;
+% T2 = 28;
 
-plot(czas,pomiary);
+plot(czas,pomiary, 'r');
+set(gcf,'position',[300, 300, 800,400])
 grid on;
 hold on
 
@@ -52,48 +72,67 @@ mian = [T1*T2, T1 + T2, 1];
 [licz_d, mian_d]=series(del_l, del_m, licz, mian);
 Inercja_II_delay = step(licz_d, mian_d, czas);
 
-plot(t,Inercja_II_delay)
+plot(czas,Inercja_II_delay, 'm')
 
 krok = czas(2) - czas(1);
 blad2 = (Inercja_II_delay' - pomiary).^2;
 calka_z_bledu = sum(blad2)
 
 lsim([1],[1 0], blad2, czas)
+legend('Charakterystyka badanego obiektu', 'Aproksymacja modelem II rzêdu z opóŸnieniem', 'Ca³ka z kwadratu b³êdu');
 
 hold off
 
 %% Aproksymacja Strejca
-%k pozostaje takie samo, zmieniam T i rz¹d aproksymacji
-plot(czas,pomiary, 'LineWidth', 1.5);
+% k pozostaje takie samo, zmieniam T i rz¹d aproksymacji
+% WARTOŒCI KOÑCOWE
+% n = 1:3;
+% T=16.5;
 
-T=17;
+% n = 1:2;
+% T=22.2;
+
+
+plot(czas,pomiary, 'r', 'LineWidth', 1.5);
+set(gcf,'position',[300, 300, 800,400])
+
+n = 1:2;
+T=22.2;
+
+colors = ['k', 'm', 'm', 'y']
 
 m=[T 1];
 m_0 = m;
 hold on
-for n = 1:4 %tutaj bedê sobie zmienia³ rz¹d aproksymacji
+for n = n
     [l_0, m_0]=series(k,m_0,1,m);
     Apr_Strejca = step(l_0,m_0,czas);
-    plot(czas,Apr_Strejca)
+    plot(czas,Apr_Strejca, colors(n))
 end
-Apr_Strejca = step(l_0,m_0,czas);
-plot(czas,Apr_Strejca)
+% Apr_Strejca = step(l_0,m_0,czas);
+% plot(czas,Apr_Strejca)
 
 blad = sum(((pomiary-Apr_Strejca').^2)*0.1)
+krok = czas(2) - czas(1);
+blad2 = (Apr_Strejca' - pomiary).^2;
+calka_z_bledu = sum(blad2)
+
+lsim([1],[1 0], blad2, czas)
+legend('Charakterystyka badanego obiektu', 'Krok 1', 'Krok 2', 'Ca³ka z kwadratu b³êdu');
 
 hold off
 
 %% Pierdo³y
 
-[y, x, t] = step([k],[T,1]);
-    plot(t,y)
-    
-    [y, x, t] = step([k],[T1*T2, T1 + T2, 1]);
-    plot(t,y)
-    
-    [del_l, del_m] = pade(T,n);
-    licz = [n];
-    mian = [T,1];
-    [licz_d, mian_d]=series(del_l, del_m, licz, mian);
-    [y,x,t]=step(licz_d, mian_d);
-    plot(t,y)
+% [y, x, t] = step([k],[T,1]);
+%     plot(t,y)
+%     
+%     [y, x, t] = step([k],[T1*T2, T1 + T2, 1]);
+%     plot(t,y)
+%     
+%     [del_l, del_m] = pade(T,n);
+%     licz = [n];
+%     mian = [T,1];
+%     [licz_d, mian_d]=series(del_l, del_m, licz, mian);
+%     [y,x,t]=step(licz_d, mian_d);
+%     plot(t,y)
